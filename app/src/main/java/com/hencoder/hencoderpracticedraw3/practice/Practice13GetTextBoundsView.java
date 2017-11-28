b40d45e87e2fefc7bf2c8635e95695c60869accb
Tconan99 <627594232@qq.com>
2017-11-28 14:01:58 +0800

    交作业

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,6 +20,13 @@ public class Practice13GetTextBoundsView extends View {
     String text6 = "â";
     int top = 200;
     int bottom = 400;
+
+    int height1 = 0;
+    int height2 = 0;
+    int height3 = 0;
+    int height4 = 0;
+    int height5 = 0;
+    int height6 = 0;
 
     public Practice13GetTextBoundsView(Context context) {
         super(context);
@@ -37,6 +45,26 @@ public class Practice13GetTextBoundsView extends View {
         paint1.setStrokeWidth(20);
         paint1.setColor(Color.parseColor("#E91E63"));
         paint2.setTextSize(160);
+
+
+        Rect bound = new Rect();
+        paint2.getTextBounds(text1, 0, text1.length(), bound);
+        height1 = bound.bottom + bound.top;
+
+        paint2.getTextBounds(text2, 0, text2.length(), bound);
+        height2 = bound.bottom + bound.top;
+
+        paint2.getTextBounds(text3, 0, text3.length(), bound);
+        height3 = bound.bottom + bound.top;
+
+        paint2.getTextBounds(text4, 0, text4.length(), bound);
+        height4 = bound.bottom + bound.top;
+
+        paint2.getTextBounds(text5, 0, text5.length(), bound);
+        height5 = bound.bottom + bound.top;
+
+        paint2.getTextBounds(text6, 0, text6.length(), bound);
+        height6 = bound.bottom + bound.top;
     }
 
     @Override
@@ -50,11 +78,12 @@ public class Practice13GetTextBoundsView extends View {
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+
+        canvas.drawText(text1, 100, middle - height1/2, paint2);
+        canvas.drawText(text2, 200, middle - height2/2, paint2);
+        canvas.drawText(text3, 300, middle - height3/2, paint2);
+        canvas.drawText(text4, 400, middle - height4/2, paint2);
+        canvas.drawText(text5, 500, middle - height5/2, paint2);
+        canvas.drawText(text6, 600, middle - height6/2, paint2);
     }
 }
